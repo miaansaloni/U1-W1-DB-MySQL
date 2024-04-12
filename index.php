d644fa7b4e09d47f4c5952c04a8a787f2499336b
Mia <155841134+miaansaloni@users.noreply.github.com>
2024-04-12 16:36:36 +0200

    adds pagination

--- index.php
+++ index.php
@@ -1,26 +1,32 @@
 <?php
 include __DIR__ . '/includes/db.php';
 
-// Inserimento dei dati nel database quando il modulo viene inviato
+$totalUsersStmt = $pdo->query('SELECT COUNT(*) FROM users');
+$totalUsers = $totalUsersStmt->fetchColumn();
+$usersPerPage = 4;
+$totalPages = ceil($totalUsers / $usersPerPage);
+$current_page = isset($_GET['page']) ? intval($_GET['page']) : 1;
+$offset = ($current_page - 1) * $usersPerPage;
+
+// Inserimento dei dati nel database quando il form viene inviato
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $name = $_POST['name'];
     $surname = $_POST['surname'];
     $age = $_POST['age'];
     $email = $_POST['email'];
 
-    // Preparazione della query di inserimento
     $stmt = $pdo->prepare("INSERT INTO users (name, surname, age, email) VALUES (?, ?, ?, ?)");
-    // Esecuzione della query
     $stmt->execute([$name, $surname, $age, $email]);
 }
 
 // SELECT DI TUTTE LE RIGHE O DI RIGHE FILTRATE PER RICERCA
-if (isset($_GET['search'])){
+if (isset($_GET['search'])) {
     $search = $_GET['search'];
-    $stmt = $pdo->prepare("SELECT * FROM users WHERE name LIKE ?");
-    $stmt->execute(["%$search%"]);
-}else{
-    $stmt = $pdo->query('SELECT * FROM users');
+    $stmt = $pdo->prepare("SELECT * FROM users WHERE name LIKE ? LIMIT ?, ?");
+    $stmt->execute(["%$search%", $offset, $usersPerPage]);
+} else {
+    $stmt = $pdo->prepare("SELECT * FROM users LIMIT ?, ?");
+    $stmt->execute([$offset, $usersPerPage]);
 }
 
 include __DIR__ . '/includes/init.php'; ?>
@@ -58,6 +64,17 @@ include __DIR__ . '/includes/init.php'; ?>
             </li>
         <?php }?>
     </ul>
+
+
+<nav aria-label="Page navigation example">
+    <ul class="pagination">
+        <?php for ($i = 1; $i <= $totalPages; $i++) {?>
+            <li class="page-item <?= $i === $current_page ? 'active' : '' ?>">
+                <a class="page-link" href="?page=<?= $i ?><?= isset($_GET['search']) ? '&search=' . $_GET['search'] : '' ?>"><?= $i ?></a>
+            </li>
+        <?php }?>
+    </ul>
+</nav>
 </div><?php
   
 include __DIR__ . '/includes/end.php';
